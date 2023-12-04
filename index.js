9f596eb2b4cdb6552592957394050a93e9f1b3bd
米田匠吾 <yoneda.shogo@gmail.com>
2023-12-04 23:40:45 +0900

    フェードインを実装しました。

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ $(".autoplay").slick({
   infinite: true,
   responsive: [{ breakpoint: 768, settings: { slidesToShow: 1 } }],
 });
+
+// 題名のフェードイン
+$(function () {
+  $(window).scroll(function () {
+    $("h3,h2").each(function () {
+      const pos = $(this).offset().top;
+      const scroll = $(window).scrollTop();
+      const windowHeight = $(window).height();
+      if (scroll > pos - windowHeight + 50) {
+        $(this).addClass("scroll");
+      }
+    });
+  });
+});
